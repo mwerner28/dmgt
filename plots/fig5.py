fc32aa4ef660a96ffe3b1dbcdb472f25b9b8bde0
Mariel Werner <mariel_werner@berkeley.edu>
2022-01-21 23:31:36 -0800

    syntax edits

--- plots/fig5.py
+++ plots/fig5.py
@@ -17,24 +17,21 @@ def plot_figure5(df,
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     
-    plot=sns.lineplot(ax=ax,
-                      data=df[['sel_rnd', 'FED_DMGT_all_acc']],
+    plot=sns.lineplot(data=df[['sel_rnd', 'FED_DMGT_all_acc']],
                       x='sel_rnd',
                       y='FED_DMGT_all_acc',
                       color=sns.color_palette('pastel')[0],
                       ci=95,
                       estimator='mean')
     
-    sns.lineplot(ax=ax,
-                 data=df[['sel_rnd', 'RAND_all_acc']],
+    sns.lineplot(data=df[['sel_rnd', 'RAND_all_acc']],
                  x='sel_rnd',
                  y='RAND_all_acc',
                  color=sns.color_palette('pastel')[2],
                  ci=95,
                  estimator='mean')
     
-    sns.lineplot(ax=ax,
-                 data=df[['sel_rnd', 'FED_DMGT_rare_acc']],
+    sns.lineplot(data=df[['sel_rnd', 'FED_DMGT_rare_acc']],
                  x='sel_rnd',
                  y='FED_DMGT_rare_acc',
                  color=sns.color_palette('muted')[0],
@@ -42,8 +39,7 @@ def plot_figure5(df,
                  ci=95,
                  estimator='mean')
     
-    sns.lineplot(ax=ax,
-                 data=df[['sel_rnd', 'RAND_rare_acc']],
+    sns.lineplot(data=df[['sel_rnd', 'RAND_rare_acc']],
                  x='sel_rnd',
                  y='RAND_rare_acc',
                  color=sns.color_palette('muted')[2],
@@ -55,7 +51,7 @@ def plot_figure5(df,
     for sel_rnd in sel_rnds:
         sizes_labels += [int(np.floor(df[df['sel_rnd']==sel_rnd]['sum_sizes'].mean()))]
     
-    round_skip=2 if dataset_name=='MNIST' else 1
+    round_skip=1 if dataset_name=='imagenet' else 2
     
     ax.xaxis.set_major_locator(ticker.FixedLocator(sel_rnds[::round_skip]))
     ax.xaxis.set_major_formatter(ticker.FixedFormatter([f'{i}'+'\n'+f'{sizes_labels[i]}' for i in sel_rnds[::round_skip]]))
@@ -71,5 +67,5 @@ def plot_figure5(df,
 
     fig.text(0.001, 0.04, 'Round\nAvg. # sel. pts', fontsize=8)
     fig.tight_layout()
-    fig.savefig(fig_dir + f'_{dataset_name}_accuracy.pdf')
+    fig.savefig(fig_dir + f'{dataset_name}_accuracy.pdf')
 
