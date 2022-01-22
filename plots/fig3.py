590e0601e2d3a466e958a15b20424d8ddd81e2cf
Mariel Werner <mariel_werner@berkeley.edu>
2022-01-21 23:27:45 -0800

    syntax edits

--- plots/fig3.py
+++ plots/fig3.py
@@ -24,31 +24,27 @@ def plot_figure3(df,
              (df['num_init_pts']==num_init_pts) &
              (df['imbal']==imbal) &
              (df['tau']==tau)][['sel_rnd',
-                                  'DMGT_all_acc',
-                                  'RAND_all_acc',
-                                  'DMGT_rare_acc',
-                                  'RAND_rare_acc',
-                                  'sum_sizes',
-                                  'trial']])
+                                'DMGT_all_acc',
+                                'RAND_all_acc',
+                                'DMGT_rare_acc',
+                                'RAND_rare_acc',
+                                'sum_sizes']])
                  
-    plot=sns.lineplot(ax=ax,
-                      data=df[df['trial'].isin(trials)][['sel_rnd', 'DMGT_all_acc']],
+    plot=sns.lineplot(data=df[['sel_rnd', 'DMGT_all_acc']],
                       x='sel_rnd',
                       y='DMGT_all_acc',
                       color=sns.color_palette('pastel')[0],
                       ci=95,
                       estimator='mean')
     
-    sns.lineplot(ax=ax,
-                 data=df[df['trial'].isin(trials)][['sel_rnd', 'RAND_all_acc']],
+    sns.lineplot(data=df[['sel_rnd', 'RAND_all_acc']],
                  x='sel_rnd',
                  y='RAND_all_acc',
                  color=sns.color_palette('pastel')[2],
                  ci=95,
                  estimator='mean')
     
-    sns.lineplot(ax=ax,
-                 data=df[df['trial'].isin(trials)][['sel_rnd', 'DMGT_rare_acc']],
+    sns.lineplot(data=df[['sel_rnd', 'DMGT_rare_acc']],
                  x='sel_rnd',
                  y='DMGT_rare_acc',
                  color=sns.color_palette('muted')[0],
@@ -56,8 +52,7 @@ def plot_figure3(df,
                  ci=95,
                  estimator='mean')
     
-    sns.lineplot(ax=ax,
-                 data=df[df['trial'].isin(trials)][['sel_rnd', 'RAND_rare_acc']],
+    sns.lineplot(data=df[['sel_rnd', 'RAND_rare_acc']],
                  x='sel_rnd',
                  y='RAND_rare_acc',
                  color=sns.color_palette('muted')[2],
@@ -69,24 +64,21 @@ def plot_figure3(df,
     for sel_rnd in sel_rnds:
         sizes_labels += [int(np.floor(df[df['sel_rnd']==sel_rnd]['sum_sizes'].mean()))]
     
-    if dataset_name=='MNIST':
-        round_skip=2
-
-        plot.set_ylabel('Accuracy', fontsize=14, labelpad=7)
-        fig.text(0.001, 0.04, 'Round\nAvg. # sel. pts', fontsize=8)
-    
-    else:
-        round_skip=1
-
-        ax.legend(labels=['DMGT: all classes',
-                          'RAND: all classes',
-                          'DMGT: rare classes',
-                          'RAND: rare classes'],
-                  loc='lower right')
-    
     ax.xaxis.set_major_locator(ticker.FixedLocator(sel_rnds[::round_skip]))
     ax.xaxis.set_major_formatter(ticker.FixedFormatter([f'{i}'+'\n'+f'{sizes_labels[i]}' for i in sel_rnds[::round_skip]]))
+   
+    round_skip=1 if dataset_name=='imagenet' else 2
+    
     plot.set_xlabel(None)
+    plot.set_ylabel('Accuracy', fontsize=14, labelpad=7)
+    
+    ax.legend(labels=['DMGT: all classes',
+                      'RAND: all classes',
+                      'DMGT: rare classes',
+                      'RAND: rare classes'],
+              loc='lower right')
+    
+    fig.text(0.001, 0.04, 'Round\nAvg. # sel. pts', fontsize=8)
     fig.tight_layout()
-    fig.savefig(fig_dir + f'_{dataset_name}_accuracy.pdf')
+    fig.savefig(fig_dir + f'{dataset_name}_accuracy.pdf')
 
